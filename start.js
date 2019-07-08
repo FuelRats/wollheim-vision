2e0d89d6a19da995f8f11a09ec7203ce7e9d0357
Chris Gårdenberg <webmaster@itssimple.se>
2019-07-08 19:21:41 +0200

    Adding changes.. because

--- start.js
+++ start.js
@@ -54,7 +54,7 @@ app.get("/upload", function(req, res) {
 });
 
 app.get("/tools", function(req, res) {
-	res.send("Not yet, but soon&trade;.");
+	res.send("Not yet, but soon&trade;. Any time now..");
 });
 
 app.get("/screenshots", function(req, res) {
